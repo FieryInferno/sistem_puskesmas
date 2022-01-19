0d9a0e8b87a3fe02d5fcacdaac2d87817bfe3590
FieryInferno <bagassetia271@gmail.com>
2022-01-19 23:06:11 +0700

    List penilaian

--- resources/views/admin/nilai/index.blade.php
+++ resources/views/admin/nilai/index.blade.php
@@ -45,58 +45,60 @@
           </button>
         </div>
       @endif
-      <div class="row">
-        <div class="col-9">
-          Persyaratan<br>
-          <span class="fa fa-star checked"></span>
-          <span class="fa fa-star checked"></span>
-          <span class="fa fa-star checked"></span>
-          <span class="fa fa-star"></span>
-          <span class="fa fa-star"></span>
-          <!-- <form class="rating">
-            <label>
-              <input type="radio" name="stars" value="1"/>
-              <span class="icon">★</span>
-            </label>
-            <label>
-              <input type="radio" name="stars" value="2"/>
-              <span class="icon">★</span>
-              <span class="icon">★</span>
-            </label>
-            <label>
-              <input type="radio" name="stars" value="3"/>
-              <span class="icon">★</span>
-              <span class="icon">★</span>
-              <span class="icon">★</span>   
-            </label>
-            <label>
-              <input type="radio" name="stars" value="4"/>
-              <span class="icon">★</span>
-              <span class="icon">★</span>
-              <span class="icon">★</span>
-              <span class="icon">★</span>
-            </label>
-            <label>
-              <input type="radio" name="stars" value="5"/>
-              <span class="icon">★</span>
-              <span class="icon">★</span>
-              <span class="icon">★</span>
-              <span class="icon">★</span>
-              <span class="icon">★</span>
-            </label>
-          </form> -->
-        </div>
-        <div class="col-3">
-          <div class="row">
-            <div class="col-3">
-              <a href="/admin/nilai/tambah" class="btn btn-success mb-3 btn-sm">Edit</a>
-            </div>
-            <div class="col-9 text-right">
-              <a href="/admin/nilai/tambah" class="btn btn-danger mb-3 btn-sm">Hapus</a>
+      @foreach ($nilai as $nilai)
+        <div class="row">
+          <div class="col-9">
+            {{ $nilai->penilaian }}<br>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star"></span>
+            <span class="fa fa-star"></span>
+            <!-- <form class="rating">
+              <label>
+                <input type="radio" name="stars" value="1"/>
+                <span class="icon">★</span>
+              </label>
+              <label>
+                <input type="radio" name="stars" value="2"/>
+                <span class="icon">★</span>
+                <span class="icon">★</span>
+              </label>
+              <label>
+                <input type="radio" name="stars" value="3"/>
+                <span class="icon">★</span>
+                <span class="icon">★</span>
+                <span class="icon">★</span>   
+              </label>
+              <label>
+                <input type="radio" name="stars" value="4"/>
+                <span class="icon">★</span>
+                <span class="icon">★</span>
+                <span class="icon">★</span>
+                <span class="icon">★</span>
+              </label>
+              <label>
+                <input type="radio" name="stars" value="5"/>
+                <span class="icon">★</span>
+                <span class="icon">★</span>
+                <span class="icon">★</span>
+                <span class="icon">★</span>
+                <span class="icon">★</span>
+              </label>
+            </form> -->
+          </div>
+          <div class="col-3">
+            <div class="row">
+              <div class="col-3">
+                <a href="/admin/nilai/tambah" class="btn btn-success mb-3 btn-sm">Edit</a>
+              </div>
+              <div class="col-9 text-right">
+                <a href="/admin/nilai/tambah" class="btn btn-danger mb-3 btn-sm">Hapus</a>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      @endforeach
     </div>
   </div>
 </div>
