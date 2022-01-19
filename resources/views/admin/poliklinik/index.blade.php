60f83dec1b3d2c2523728d0dde21ce64afa50252
FieryInferno <bagassetia271@gmail.com>
2022-01-19 07:36:26 +0700

    Add cetak antrian

--- resources/views/admin/poliklinik/index.blade.php
+++ resources/views/admin/poliklinik/index.blade.php
@@ -75,5 +75,42 @@
       </div>
     </div>
   </div>
+  <div class="row">
+    <div class="col-3">
+      <div class="card">
+        <div class="card-body text-center">
+          Antrian Saat Ini<br>
+          <h1>10</h1>
+          Cetak Antrian <a href="#" data-toggle="modal" data-target="#cetakAntrian">Disini</a>
+          <!-- Modal -->
+          <div class="modal fade" id="cetakAntrian" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">
+                    PUSKESMAS NAGRAK<br> 
+                    Jl. Sinagar-Munjul, Nagrak Utara, Kec. Nagrak, 
+                    Sukabumi Regency, Jawa Barat 43356
+                  </h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  No. antrian anda
+                  <div class="text-center">
+                    <h1>10</h1>
+                  </div>
+                </div>
+                <div class="modal-footer">
+                  Silahkan tunggu hingga nomor anda dipanggil. <a href="">Print</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 @endsection
