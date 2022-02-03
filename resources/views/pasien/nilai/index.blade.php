bec77235dc809b4bf8fa95ef98275b76f69aee37
FieryInferno <bagassetia271@gmail.com>
2022-02-03 11:04:25 +0700

    Fix nilai pasien

--- resources/views/pasien/nilai/index.blade.php
+++ resources/views/pasien/nilai/index.blade.php
@@ -18,33 +18,33 @@
             </button>
           </div>
         @endif
-        @foreach ($nilai as $nilai)
-          {{ $nilai->penilaian }}<br>
+        @foreach ($penilaian as $key => $value)
+          {{ $value["penilaian"] }}<br>
           <div class="rating">
             <label>
-              <input type="radio" name="nilai[{{ $nilai->id }}]" value="1"/>
+              <input type="radio" name="nilai[{{ $key }}]" value="1"/>
               <span class="icon">★</span>
             </label>
             <label>
-              <input type="radio" name="nilai[{{ $nilai->id }}]" value="2"/>
+              <input type="radio" name="nilai[{{ $key }}]" value="2"/>
               <span class="icon">★</span>
               <span class="icon">★</span>
             </label>
             <label>
-              <input type="radio" name="nilai[{{ $nilai->id }}]" value="3"/>
+              <input type="radio" name="nilai[{{ $key }}]" value="3"/>
               <span class="icon">★</span>
               <span class="icon">★</span>
               <span class="icon">★</span>   
             </label>
             <label>
-              <input type="radio" name="nilai[{{ $nilai->id }}]" value="4"/>
+              <input type="radio" name="nilai[{{ $key }}]" value="4"/>
               <span class="icon">★</span>
               <span class="icon">★</span>
               <span class="icon">★</span>
               <span class="icon">★</span>
             </label>
             <label>
-              <input type="radio" name="nilai[{{ $nilai->id }}]" value="5"/>
+              <input type="radio" name="nilai[{{ $key }}]" value="5"/>
               <span class="icon">★</span>
               <span class="icon">★</span>
               <span class="icon">★</span>
